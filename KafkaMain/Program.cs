899a6ff1b1e96f7c6f64e886681ac89122f76f16
Tomer <tomer.cohen243@gmail.com>
2024-10-23 13:49:51 +0300

    [init] created project

---
+++ KafkaMain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
