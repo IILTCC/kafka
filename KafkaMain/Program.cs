60cf7477e1f9097eed13aeff348b04eb9bc326af
Tomer <tomer.cohen243@gmail.com>
2024-10-23 15:59:12 +0300

    [feat] intial starting of kafka server and zookeeper

--- KafkaMain/Program.cs
+++ KafkaMain/Program.cs
@@ -6,7 +6,7 @@ namespace KafkaMain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ServerStartup.StartKafkaServer();
         }
     }
 }
---
+++ KafkaMain/ServerStartup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace KafkaMain
+{
+    public static class ServerStartup
+    {
+        public static void StartServer()
+        {
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/k cd C:\\kafka",
+                    RedirectStandardInput = true,      // Redirect input so we can send commands
+
+                    UseShellExecute = false,           // Do not use shell execute
+                    CreateNoWindow = false
+                }
+            };
+            Console.WriteLine("going to start");
+            process.Start();
+            process.StandardInput.WriteLine(@".\bin\windows\zookeeper-startup.bat .\config\zookeeper.properties");
+            //process.StandardInput.WriteLine("cd c");
+            //process.StandardInput.WriteLine("cd kafka");
+            //string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            //Console.WriteLine(output);
+        }
+    }
+}
